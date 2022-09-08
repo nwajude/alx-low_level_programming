fdab4b5919e1faeaf555f998b2363d2eaf0a3c27
nwajude <nwabuddikejude1@gmail.com>
2022-09-08 12:44:53 -0700

    Cfour

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success) 
- */
+ *  * main - Entry point
+ *   *
+ *    * Return: Always O (Success)
+ *     */
 
-int main(void) 
-{ 
-	puts("\"Programming is like building a multilingual puzzle");
-       	return (0);
-} 
+int main(void)
+{
+	        puts("\"Programming is like building a multilingual puzzle");
+		        return (0);
+}
