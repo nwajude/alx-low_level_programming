09983e947e199a97631741f84ca0923c7af7df49
nwajude <nwabuddikejude1@gmail.com>
2022-09-14 03:32:46 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,33 +1,26 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ *  * print_sign - prints the sign of a number
+ *   * @n: positive, zero or negative integer
+ *    * Return: 1 if it's positive, 0 if it's negative and -1 if it's negative
  */
-int main(void)
-{
-	int r;
 
-	r = print_sign(98);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0xff);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(-1);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
 }
