d1badcda8d20fba40ec4a7d51c99022a219ed589
nwajude <nwabuddikejude1@gmail.com>
2022-12-12 21:21:30 -0800

    fly

--- 0x18-dynamic_libraries/3-puts.c
+++ 0x18-dynamic_libraries/3-puts.c
@@ -1,19 +1,16 @@
 #include "main.h"
-
 /**
- *  * _puts - Write a function that prints a string, followed
- *   * by a new line, to stdout.
- *    * @str: an input string
- *     * Return: Nothing
- */
+ *  *_puts - prints a string
+ *   * @str: string to print
+ *    *
+ *     * Description: prints a string
+ *      * On success: return the number of characters printed
+ *       */
+
 void _puts(char *str)
 {
-	int i = 0;
+	while (*str)
+		_putchar(*str++);
 
-	while (str[i] != '\0')
-	{
-		putchar(str[i]);
-		i++;
-	}
-	putchar('\n');
+	_putchar('\n');
 }
